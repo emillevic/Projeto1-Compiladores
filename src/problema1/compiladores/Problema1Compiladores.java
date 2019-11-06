4c05993ff0771d89b6fcb448dd50773d487272be
emillevic <emillesampaio2013@gmail.com>
2019-11-06 19:04:50 -0300

    First commit

---
+++ src/problema1/compiladores/Problema1Compiladores.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problema1.compiladores;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Mille
+ */
+public class Problema1Compiladores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public static void leitor(String path) throws IOException {
+        BufferedReader buffRead = new BufferedReader(new FileReader(path));
+        String linha = "";
+        while (true) {
+            if (linha != null) {
+                System.out.println(linha);
+ 
+            } else
+                break;
+            linha = buffRead.readLine();
+        }
+        buffRead.close();
+    }
+ 
+    public static void escritor(String path) throws IOException {
+        BufferedWriter buffWrite = new BufferedWriter(new FileWriter(path));
+        String linha = "";
+        Scanner in = new Scanner(System.in);
+        System.out.println("Escreva algo: ");
+        linha = in.nextLine();
+        buffWrite.append(linha + "\n");
+        buffWrite.close();
+    }
+}
