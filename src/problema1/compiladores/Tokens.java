0bf73b732f4fbea76384f159778d2d6320e55210
emillevic <emillesampaio2013@gmail.com>
2019-11-12 10:00:13 -0300

    Adicionei classe de tokens

---
+++ src/problema1/compiladores/Tokens.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problema1.compiladores;
+
+/**
+ *
+ * @author Mille
+ */
+public class Tokens {
+    
+    private final String tipo;
+    private final int linha;
+    private final char[] lexema;
+
+    public Tokens(int linha, String tipo, char[] lexema) {
+        this.tipo = tipo;
+        this.linha = linha;
+        this.lexema = lexema;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+    public int getLinha() {
+	return linha;
+    }
+    public char[] getLexema() {
+	return lexema;
+    }
+}
